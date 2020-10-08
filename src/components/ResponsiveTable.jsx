88d571e9d3024ace9a231716118fd29d674f5b9a
karen <gallegokaren1897@gmail.com>
2020-10-08 19:09:21 -0400

    create responsive table with stub form

--- src/components/ResponsiveTable.jsx
+++ src/components/ResponsiveTable.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Table from "./Table";
+import { Grid } from 'antd';
+
+const { useBreakpoint } = Grid;
 
 const ResponsiveTable = ({ data, schema, handleAction }) => {
+  const screens = useBreakpoint();
+  useEffect(() => {console.log({screens})}, [screens])
+
+  if (!screens.md) {
+    return (
+      <div>this is a form</div>
+    )
+  }
+
   return (
-    <>
-      <Table
-        data={data}
-        schema={schema}
-        handleAction={handleAction}
-      />
-    </>
+    <Table
+      data={data}
+      schema={schema}
+      handleAction={handleAction}
+    />
   )
 };
 
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,26 +1,18 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import ReactTable from 'react-table-v6';
 import 'react-table-v6/react-table.css';
 
-import { Grid } from 'antd';
 import CellFactory from "./CellFactory";
 
-const { useBreakpoint } = Grid;
-
-const Table = ({ data, schema, handleAction }) => {
-
-  const screens = useBreakpoint();
-
-  useEffect(() => {console.log({screens});}, [])
-
+const Table = ({data, schema, handleAction}) => {
   const columns = schema.map((item) => ({
     Header: item.label,
     accessor: item.accessor,
-    Cell: (cell) => <CellFactory item={item} cell={cell} handleAction={handleAction} />
+    Cell: (cell) => <CellFactory item={item} cell={cell} handleAction={handleAction}/>
   }))
 
   return (
-    <ReactTable columns={columns} data={data} />
+    <ReactTable columns={columns} data={data}/>
   )
 }
 
