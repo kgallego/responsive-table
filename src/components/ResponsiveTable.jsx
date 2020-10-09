621206358285990e0fab4a1fdd116a6be2b89088
karen <gallegokaren1897@gmail.com>
2020-10-09 12:25:37 -0400

    fix bug with id defaulting to initial state - used useRef

--- src/components/ResponsiveTable.jsx
+++ src/components/ResponsiveTable.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import Table from "./Table";
 import {Grid} from 'antd';
 import List from "./List";
@@ -7,18 +7,19 @@ import Modal from "./Modal";
 const { useBreakpoint } = Grid;
 
 const ResponsiveTable = ({ title, data, schema, handleAction }) => {
-  const [id, setId] = useState(data[0].id);
-  const [currentData, setCurrentData] = useState(data[0]);
+  const idRef = useRef(data[0].id);
+  const [currentData, setCurrentData] = useState(data[idRef.current]);
   const [visible, setVisible] = useState(false);
   const screens = useBreakpoint();
 
+
   useEffect(() => {
-    setCurrentData(data.find(item => item.id === id))
+    setCurrentData(data.find(item => item.id === idRef.current))
   }, [data]) // not sure why this useeffect was necessary, when data comes in it should rerender; is it because of data being an array of objects?
 
   const handleShowInfo = (_id) => {
     setCurrentData(data.find(item => item.id === _id));
-    setId(id)
+    idRef.current = _id;
     setVisible(true);
   }
 
