37401e9b4f79299352dd04f42b648406f31d4cbb
karen <gallegokaren1897@gmail.com>
2020-10-08 23:14:03 -0400

    fix rendering issue when making actions on the modal

--- src/components/ResponsiveTable.jsx
+++ src/components/ResponsiveTable.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Table from "./Table";
 import {Grid} from 'antd';
 import List from "./List";
@@ -7,12 +7,18 @@ import Modal from "./Modal";
 const { useBreakpoint } = Grid;
 
 const ResponsiveTable = ({ title, data, schema, handleAction }) => {
+  const [id, setId] = useState(data[0].id);
   const [currentData, setCurrentData] = useState(data[0]);
   const [visible, setVisible] = useState(false);
   const screens = useBreakpoint();
 
-  const handleShowInfo = (id) => {
-    setCurrentData(data.find(item => item.id === id));
+  useEffect(() => {
+    setCurrentData(data.find(item => item.id === id))
+  }, [data]) // not sure why this useeffect was necessary, when data comes in it should rerender; is it because of data being an array of objects?
+
+  const handleShowInfo = (_id) => {
+    setCurrentData(data.find(item => item.id === _id));
+    setId(id)
     setVisible(true);
   }
 
